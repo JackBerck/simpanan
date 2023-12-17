ac50e89d1beae811b8c774bc39e2a6342ff8f269
Jack Berck <68431607+JackBerck@users.noreply.github.com>
2023-12-17 23:24:20 +0700

    Update uas_2016.cpp

--- uas_2016.cpp
+++ uas_2016.cpp
@@ -59,3 +59,24 @@ int main()
 
     return 0;
 }
+
+// Soal 3
+Rekursif adalah suatu konsep dalam pemrograman di mana suatu fungsi dapat memanggil dirinya sendiri.
+
+#include <iostream>
+
+using namespace std;
+
+void countdown(int n) {
+    if (n > 0) {
+        cout << n << " ";
+        countdown(n - 1);
+    } else {
+        cout << "Liftoff!" << endl;
+    }
+}
+
+int main() {
+    countdown(5);
+    return 0;
+}
