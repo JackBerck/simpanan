c19decca523a8feba02bfc63c7d355dacc651efc
Jack Berck <68431607+JackBerck@users.noreply.github.com>
2023-12-17 23:21:45 +0700

    Update uas_2019.cpp

--- uas_2019.cpp
+++ uas_2019.cpp
@@ -1,3 +1,26 @@
+// Soal 1
+Pointer adalah variabel yang menyimpan alamat memori dari variabel lain sehingga dapat mengakses atau memanipulasi nilai yang disimpan di lokasi memori tertentu.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int angka = 42;
+    int *pointerAngka = &angka;
+
+    cout << "Nilai dari angka: " << angka << endl;
+    cout << "Alamat memori dari angka: " << &angka << endl;
+    cout << "Nilai yang disimpan di alamat pointerAngka: " << *pointerAngka << endl;
+
+    return 0;
+}
+
+// Soal 2
+Rekursif adalah suatu konsep dalam pemrograman di mana suatu fungsi dapat memanggil dirinya sendiri.
+Jenis-jenis rekursif:
+1. Rekursi langsung, suatu fungsi memanggil dirinya sendiri secara langsung.
+2. Rekursi tidak langsung, dua atau lebih fungsi saling memanggil untuk mencapai solusi.
+
 // Soal 3
 
 #include <iostream>
